0edc2046072f2acc2f0ae676026e05f40498b64b
이혜성 <hyeseong43@gmail.com>
2021-02-22 00:02:17 +0900

    [BlogAPI] Permissions, User Authenticaion, Viesets&Routers, Schemas and Documentation

---
+++ posts/serializers.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+from .models import Post
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username')
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'author', 'title', 'body', 'created_at')
+        model = Post
+
